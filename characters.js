5246e0a353106ece47132d22eb974522ce65f70f
rimuru926 <116069307+rimuru926@users.noreply.github.com>
2026-04-05 23:40:51 +0300

    Update characters.js

--- characters.js
+++ characters.js
@@ -5,6 +5,12 @@ const CHARACTERS = [
     image: "https://static.wikia.nocookie.net/characterprofile/images/5/50/Roronoa_Zoro.png",
     quote: "Nothing happened.",
   }
+  {
+    name: "Son goku",
+    series: "Dragon ball",
+    image: "https://www.google.com/imgres?q=goku&imgurl=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fultradragonball%2Fimages%2F1%2F12%2F12345.png%2Frevision%2Flathttps://static.wikia.nocookie.net/ultradragonball/images/1/12/12345.png/revision/latest/scale-to-width-down/1000?cb=20130726100006scale-to-width-down/1000?cb=20250723190513",
+    quote: "Hey its me goku",
+  },
 ];
 
 // This line ensures the index.html can see the list
