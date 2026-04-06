1ebb0832aa665695b4ae76fce68ac347ae07276a
rimuru926 <116069307+rimuru926@users.noreply.github.com>
2026-04-06 11:43:57 +0300

    Update characters.js

--- characters.js
+++ characters.js
@@ -18,8 +18,8 @@ const CHARACTERS = [
   // through the site and generate the code automatically.
 
   {
-    name: "Jonathan joestar",
-    series: "Jojo's bizzare adventure",
+    name: "Jonathan Joestar",
+    series: "Jojo's Bizzare Adventure",
     image: "https://jojowiki.com/File:Jonathan_Infobox_Anime.png",
     quote: "I strive to be a true gentleman! There are times when a gentleman has to be courageous and fight, even when his opponent is bigger than he is and he knows he's going to lose!",
     quote2: "Th- That Stone Mask... It's given birth to a monster! Frankly, I'm scared! But I can't let you remain in this world, ***! I'll put an end to you! I never expected the Stone Mask that I was inquisitively researching to be a tool for creating demons... It's my fault... I have to fight!",
